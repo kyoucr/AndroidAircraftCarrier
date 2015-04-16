9e1af28fac52d35cde31e104cd4e07b8257c8a80
quanmin.li <quanmin.li@dianping.com>
2015-04-16 11:35:55 +0800

    【Lab】Activity跳转的生命周期

--- AndroidAircraftCarrier/app/src/main/java/com/footprint/androidaircraftcarrier/lab/LabActivity.java
+++ AndroidAircraftCarrier/app/src/main/java/com/footprint/androidaircraftcarrier/lab/LabActivity.java
@@ -1,5 +1,7 @@
 package com.footprint.androidaircraftcarrier.lab;
 
+import android.util.Log;
+
 import com.footprint.androidaircraftcarrier.main.ActivityItem;
 import com.footprint.androidaircraftcarrier.main.FPListActivity;
 
@@ -15,6 +17,34 @@ public class LabActivity extends FPListActivity {
         List<ActivityItem> labList = new ArrayList<ActivityItem>();
         ActivityItem listItem = new ActivityItem("ListView的Header测试", "lab://listhead");
         labList.add(listItem);
+
+        listItem = new ActivityItem("Activity跳转时生命周期测试", "lab://lifecycle");
+        labList.add(listItem);
+
         return labList;
+    }
+
+    @Override
+    protected void onStart() {
+        Log.e("FP", "Lab-Start");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.e("FP", "Lab-Resume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.e("FP", "Lab-Pause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.e("FP", "Lab-Stop");
+        super.onStop();
     }
 }
---
+++ AndroidAircraftCarrier/app/src/main/java/com/footprint/androidaircraftcarrier/lab/LifeCycle.java
@@ -0,0 +1,56 @@
+package com.footprint.androidaircraftcarrier.lab;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.footprint.androidaircraftcarrier.R;
+/**
+ * 【实验】从A-Activity跳转到B-Activity的时候生命周期如下：
+ * A-Pause，B-Start，B-Resume，A-Stop
+ * 从B-Activity回到A-Activity的生命周期如下：
+ * B-Pause，A-Start，A-Resume，B-Stop，B-Destroy
+ * 【结论】即将显示的Activity在显示完成（即Resume）之后，之前的Activity才会Stop以及可能的Destroy
+ * 【适用】服务的绑定和非绑定，官方文档建议：
+ * You should usually not bind and unbind during your activity's onResume() and onPause()
+ * 而应该在其余两对生命周期中完成bind和unbind，原因在于：
+ * 在其余两对生命周期中执行动作不会引起Service的反复创建和销毁！
+ * */
+public class LifeCycle extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_life_cycle);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.e("FP", "LifeCycle-Start");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.e("FP", "LifeCycle-Resume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.e("FP", "LifeCycle-Pause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.e("FP", "LifeCycle-Stop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.e("FP", "LifeCycle-Destroy");
+        super.onDestroy();
+    }
+}
