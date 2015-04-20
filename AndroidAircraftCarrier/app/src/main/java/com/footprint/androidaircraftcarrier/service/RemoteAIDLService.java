c31f02027c34bd23c1e03bb7ac1956c283c85aee
quanmin.li <quanmin.li@dianping.com>
2015-04-20 15:59:40 +0800

    【Service】注释

--- AndroidAircraftCarrier/app/src/main/java/com/footprint/androidaircraftcarrier/service/RemoteAIDLService.java
+++ AndroidAircraftCarrier/app/src/main/java/com/footprint/androidaircraftcarrier/service/RemoteAIDLService.java
@@ -8,6 +8,13 @@ import com.footprint.androidaircraftcarrier.IMyAidlInterface;
 
 /**
  * Created by liquanmin on 15/4/20.
+ *
+ * AIDL的使用方法：
+ * 1）建立一个aidl文件（app右键——>New——>AIDL）
+ * 2）Run——>Make Project
+ * 之后就会生成对应的.java文件，Project模式下，app_build_generated_source_aidl_debug
+ * 下面会看到对应生成的接口文件
+ * 之后的使用过程详见代码
  */
 public class RemoteAIDLService extends Service{
 
